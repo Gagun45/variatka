16e9f5c59045260cc9c2f58a4bacd805f1a77983
Gagun45 <selyanchyn45@gmail.com>
2026-06-17 01:16:55 +0300

    searchbar refactor

--- components/search-bar/SearchBar.tsx
+++ components/search-bar/SearchBar.tsx
@@ -3,6 +3,11 @@ import { useIngredients } from "@/features/ingredient/hooks/useIngredients";
 import { useRecipes } from "@/features/recipe/hooks/useRecipes";
 import { useSearch } from "@/prisma/store/search";
 import { useMemo, useState } from "react";
+import { Button, buttonVariants } from "../ui/button";
+import { Card, CardContent } from "../ui/card";
+import { Badge } from "../ui/badge";
+import { frontendUrls } from "@/lib/urls";
+import Link from "next/link";
 
 const SearchBar = () => {
   const query = useSearch((s) => s.query);
@@ -13,25 +18,41 @@ const SearchBar = () => {
   const [open, setOpen] = useState(false);
 
   const suggestions = useMemo(() => {
-    if (!query) return [];
-
     const q = query.toLowerCase();
 
     return [
       ...ingredients
         .filter((i) => i.title.toLowerCase().includes(q))
+        .sort((a, b) => a.title.localeCompare(b.title))
         .slice(0, 5)
-        .map((i) => ({ id: i.id, title: i.title, type: "ingredient" })),
+        .map((i) => ({
+          id: i.id,
+          title: i.title,
+          type: "ingredient",
+        })),
 
       ...recipes
         .filter((r) => r.title.toLowerCase().includes(q))
+        .sort((a, b) => a.title.localeCompare(b.title))
         .slice(0, 5)
-        .map((r) => ({ id: r.id, title: r.title, type: "recipe" })),
+        .map((r) => ({
+          id: r.id,
+          title: r.title,
+          type: "recipe",
+        })),
     ];
   }, [query, ingredients, recipes]);
 
   return (
-    <div className="relative flex w-full">
+    <div
+      className="relative flex w-full"
+      onBlur={(e) => {
+        // if focus moves outside the whole wrapper → close
+        if (!e.currentTarget.contains(e.relatedTarget as Node)) {
+          setOpen(false);
+        }
+      }}
+    >
       <Input
         value={query}
         className="w-full"
@@ -42,28 +63,52 @@ const SearchBar = () => {
           setOpen(true);
         }}
         onFocus={() => setOpen(true)}
-        onBlur={() => setOpen(false)}
       />
-      {open && suggestions.length > 0 && (
-        <div className="absolute w-fit top-full rounded-md border bg-background shadow-lg z-50">
-          {suggestions.map((item) => (
-            <div
-              key={`${item.type}-${item.id}`}
-              className="px-3 py-2 space-x-2 text-sm hover:bg-muted cursor-pointer"
-              onMouseDown={() => {
-                setQuery(item.title);
-                setOpen(false);
-              }}
-            >
-              <span>{item.title}</span>
-              <span
-                className={`text-xs px-2 rounded-md text-muted-foreground ${item.type === "recipe" ? "bg-accent" : ""}`}
-              >
-                {item.type}
-              </span>
+
+      {open && (
+        <Card className="absolute top-full z-50 mt-2 w-full py-1 shadow-lg">
+          <CardContent className="p-1">
+            <div className="h-fit space-y-2">
+              {suggestions.map((item) => (
+                <Button
+                  variant={"ghost"}
+                  className="w-full"
+                  key={`${item.type}-${item.id}`}
+                  onClick={() => {
+                    setQuery(item.title);
+                    setOpen(false);
+                  }}
+                >
+                  {/* LEFT SIDE */}
+                  <div className="flex items-center w-full justify-between gap-2 min-w-0">
+                    <p className="truncate text-sm font-medium">{item.title}</p>
+                    {item.type === "recipe" && <Badge>R</Badge>}
+                  </div>
+
+                  {/* RIGHT SIDE */}
+
+                  <Link
+                    href={
+                      item.type === "recipe"
+                        ? frontendUrls.recipes.view(item.id)
+                        : frontendUrls.ingredients.view(item.id)
+                    }
+                    className={buttonVariants({
+                      variant: "outline",
+                      size: "sm",
+                    })}
+                    onClick={() => {
+                      setQuery(item.title);
+                      setOpen(false);
+                    }}
+                  >
+                    Go to
+                  </Link>
+                </Button>
+              ))}
             </div>
-          ))}
-        </div>
+          </CardContent>
+        </Card>
       )}
     </div>
   );
