3aadb62f4e321c09a3b36e51219b6aedff951313
Gagun45 <selyanchyn45@gmail.com>
2026-06-20 13:25:37 +0300

    tooltip added

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -4,23 +4,26 @@ import { ThemeProvider } from "./ThemeProvider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import QueryProvider from "./QueryProvider";
 import { AuthProvider } from "./AuthProvider";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <AuthProvider>
-      <QueryProvider>
-        <SidebarProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </SidebarProvider>
-      </QueryProvider>
-    </AuthProvider>
+    <TooltipProvider>
+      <AuthProvider>
+        <QueryProvider>
+          <SidebarProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </SidebarProvider>
+        </QueryProvider>
+      </AuthProvider>
+    </TooltipProvider>
   );
 };
 
