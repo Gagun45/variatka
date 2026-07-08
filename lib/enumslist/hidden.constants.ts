96ca48794cebc9b417a28403ea3a2ed45e8ad7a7
Gagun45 <selyanchyn45@gmail.com>
2026-07-08 20:08:24 +0300

    icons to filters added

--- lib/enumslist/hidden.constants.ts
+++ lib/enumslist/hidden.constants.ts
@@ -1,9 +1,24 @@
+import { Eye, EyeOff, Layers } from "lucide-react";
 import { IOption } from "../types";
 
 export type IRecipeHiddenFilter = "all" | "hidden" | "visible";
 
 export const RECIPE_HIDDEN_FILTER_OPTIONS: IOption<IRecipeHiddenFilter>[] = [
-  { value: "all", label: "All" },
-  { value: "hidden", label: "Hidden" },
-  { value: "visible", label: "Visible" },
+  {
+    value: "all",
+    label: "All",
+    icon: Layers,
+  },
+  {
+    value: "hidden",
+    label: "Hidden",
+    icon: EyeOff,
+    iconClassName: "text-orange-500",
+  },
+  {
+    value: "visible",
+    label: "Visible",
+    icon: Eye,
+    iconClassName: "text-green-500",
+  },
 ];
