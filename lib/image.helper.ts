197af88b7144bcd04d514a6feef5a1a5c948121b
Gagun45 <selyanchyn45@gmail.com>
2026-07-12 21:35:20 +0300

    poster changed

--- lib/image.helper.ts
+++ lib/image.helper.ts
@@ -1,5 +1,5 @@
 export const getImageUrl = (imageKey?: string | null, version?: number) => {
-  if (!imageKey) return "/default-poster.png";
+  if (!imageKey) return "/default-poster-new.png";
 
   return `${process.env.NEXT_PUBLIC_CDN_URL}/${imageKey}?v=${version ?? 0}`;
 };
